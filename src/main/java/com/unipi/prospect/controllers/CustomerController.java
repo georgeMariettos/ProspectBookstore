9330f411323455a9ce9959abacc83dcd4f933da3
PotatoCrafter <thanasisgousias2004@gmail.com>
2025-05-19 00:27:31 +0300

    added static html pages removed default security added basic controllers for admin, author and customer

--- src/main/java/com/unipi/prospect/controllers/CustomerController.java
+++ src/main/java/com/unipi/prospect/controllers/CustomerController.java
@@ -1,12 +1,28 @@
 package com.unipi.prospect.controllers;
 
+import com.unipi.prospect.users.Customer;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/customer")
 public class CustomerController {
+
+
+    @PostMapping("/register")
+    public String registerCustomer(@RequestParam String username,
+                                   @RequestParam String password,
+                                   @RequestParam String name,
+                                   @RequestParam String address) {
+
+
+        Customer customer = new Customer(username, password, name, address);
+
+        return "redirect:/index.html";
+    }
 
     @GetMapping
     public String CustomerPage(){
