577a6786793bae19f7288863307f2c22476bc937
Geo <gmariettos2004@gmail.com>
2025-05-31 18:39:58 +0300

    Fixed DB issue

---
+++ src/main/java/com/unipi/prospect/controllers/TestController.java
@@ -0,0 +1,19 @@
+package com.unipi.prospect.controllers;
+
+import com.unipi.prospect.db.users.AdminDao;
+import com.unipi.prospect.users.Admin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test")
+public class TestController {
+
+    @GetMapping
+    public String test(){
+        Admin admin = new Admin("admin", "123", "a", "b", true);
+        System.out.println(new AdminDao().insert(admin));
+        return "redirect:/index.html";
+    }
+}
--- src/main/java/com/unipi/prospect/db/users/AdminDao.java
+++ src/main/java/com/unipi/prospect/db/users/AdminDao.java
@@ -13,6 +13,8 @@ public class AdminDao implements UserDao<Admin>{
             conn = DriverManager.getConnection("jdbc:sqlite:userData.sqlite");
         } catch (SQLException e) {
             throw new RuntimeException(e);
+        }catch (RuntimeException e){
+            throw new RuntimeException(e);
         }
     }
 
